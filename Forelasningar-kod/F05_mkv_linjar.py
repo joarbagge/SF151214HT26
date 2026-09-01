db064235fce6cdd29dd57e6b07792644eb2760e5
Joar Bagge <joar.bagge@gmail.com>
2026-09-02 01:12:44 +0200

    Frl 5 (innan)

---
+++ Forelasningar-kod/F05_mkv_linjar.py
@@ -0,0 +1,22 @@
+'''
+Exempel från Föreläsning 5 (2026-09-02)
+
+Exempel 4.8 i Sauer
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+t = np.array([1950, 1955, 1960, 1965, 1970, 1975, 1980])
+y = np.array([53.05, 73.04, 98.31, 139.78, 193.48, 260.20, 320.39])
+
+# Se tavelanteckningar för linjära ersättningsmodellen!
+# Bestäm linjära modellens koefficienter:
+...
+
+# Omvandla till värden på a och b:
+...
+
+# Plotta datapunkterna och modellen:
+...
+plt.show()
